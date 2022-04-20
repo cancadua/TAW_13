bf2f5e31e0f4673e36feedbf2875fe6501d20714
cancadua <33159@student.pwsztar.edu.pl>
2022-04-20 09:39:29 +0200

    L13

---
+++ app/REST/routes.js
@@ -0,0 +1,13 @@
+import userEndpoint from './user.endpoint';
+import postEndpoint from './post.endpoint';
+
+const routes = function (router) {
+    userEndpoint(router);
+    postEndpoint (router);
+};
+
+export default routes;
+
+
+
+
